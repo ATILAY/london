fd1d07b1594bc11adbe4fce2166ab7ee8665b2d1
ATILAY <atilaymayadag@gmail.com>
2022-01-20 18:10:44 +0300

    feat/ sign added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <div className='big-comp-margin-x'>
-            <h1 className='app-title'>Choose your Helper fast!</h1>
+            <h1 className='app-title'>Choose your Helper fast!!</h1>
         </div>
     )
 }
